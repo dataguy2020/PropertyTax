2e6b744fbc8c7437bee45d882fb1747e16d05311
Mike Brown <mikeabrown2020@gmail.com>
2023-01-18 15:30:37 -0500

    Update tanyardsprings.py

--- tanyardsprings.py
+++ tanyardsprings.py
@@ -4,14 +4,15 @@
 # pip install pandas
 # pip install sodapy
 
-import pandas as pd
-from sodapy import Socrata
 import datetime as dt
-from babel.numbers import format_currency
 
-#importing personal modules
-from functions import  *
 from auth import applicationtoken
+from babel.numbers import format_currency
+#importing personal modules
+from functions import *
+import pandas as pd
+import seaborn as sns
+from sodapy import Socrata
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
@@ -31,15 +32,15 @@ results = client.get("q4mw-f34p", limit=3000)
 results_df = pd.DataFrame.from_records(results)
 results_df.to_csv('results_df.csv')
 
-
 #creating dataframe + adding first field from the originating data frame
 cleandata = results_df[['legal_description_line_2_mdp_field_legal2_sdat_field_18']].copy()
 
 #changing column name in data frame
-cleandata.rename(columns = {'legal_description_line_2_mdp_field_legal2_sdat_field_18':'address'}, inplace = True)
+cleandata.rename(columns={'legal_description_line_2_mdp_field_legal2_sdat_field_18': 'address'}, inplace=True)
 
 #copying over columns from originating data frame
 cleandata['accountnumber'] = results_df['record_key_account_number_sdat_field_3']
+cleandata['opendataupdate'] = results_df['date_of_most_recent_open_data_portal_record_update']
 cleandata['key'] = cleandata['accountnumber']
 cleandata['housetype'] = results_df['mdp_street_address_type_code_mdp_field_resityp']
 cleandata['landuse'] = results_df['land_use_code_mdp_field_lu_desclu_sdat_field_50']
@@ -53,6 +54,36 @@ cleandata['yearbuilt'] = results_df['c_a_m_a_system_data_year_built_yyyy_mdp_fie
 cleandata['datepurchased'] = results_df['sales_segment_1_transfer_date_yyyy_mm_dd_mdp_field_tradate_sdat_field_89']
 cleandata['zoning'] = results_df['zoning_code_mdp_field_zoning_sdat_field_45']
 
+cleandata['box2'] = results_df['current_assessment_year_total_assessment_sdat_field_172']
+cleandata['box4'] = results_df['prior_assessment_year_total_assessment_sdat_field_161']
+cleandata['box5'] = results_df['current_cycle_data_land_value_mdp_field_names_nfmlndvl_curlndvl_and_sallndvl_sdat_field_164']
+cleandata['box6'] = results_df['current_cycle_data_improvements_value_mdp_field_names_nfmimpvl_curimpvl_and_salimpvl_sdat_field_165']
+cleandata['box8'] = cleandata['box2']
+
+#Conerting all to numeric
+cleandata['box2'] = pd.to_numeric(cleandata['box2'])
+cleandata['box4'] = pd.to_numeric(cleandata['box4'])
+cleandata['box5'] = pd.to_numeric(cleandata['box5'])
+cleandata['box6'] = pd.to_numeric(cleandata['box6'])
+cleandata['box8'] = pd.to_numeric(cleandata['box8'])
+
+cleandata['box7'] = (cleandata['box5'] + cleandata['box6'])
+cleandata['box9a'] = ((cleandata['box7'] - cleandata['box4'])/3)
+cleandata['box9'] = (cleandata['box9a'] + cleandata['box8'])
+
+cleandata['box10'] = cleandata['box7']
+cleandata['box7'] = pd.to_numeric(cleandata['box7'])
+cleandata['box9'] = pd.to_numeric(cleandata['box9'])
+cleandata['box10'] = pd.to_numeric(cleandata['box10'])
+
+
+
+#DEBUGGING
+#print columns & types
+#print(cleandata.dtypes)
+
+cleandata['totalchange'] = cleandata['box7'] - cleandata['box4']
+
 #converting
 cleandata['assessmentyear'] = [dt.datetime.strptime(x, '%Y').year for x in cleandata['assessmentyear']]
 cleandata['accountnumber'] = pd.to_numeric(cleandata['accountnumber'])
@@ -61,16 +92,35 @@ cleandata['key'] = pd.to_numeric(cleandata['key'])
 #setting index
 cleandata = cleandata.set_index('key')
 
-#formatting the data a bit further   
+#converting conditions
 cleandata['owneroccupancycode'] = cleandata['owneroccupancycode'].apply(owneroccupancycondition)
-cleandata['yearbuilt'] = cleandata['yearbuilt'].apply(yearcondition)
-cleandata['homesteadcreditqualificationcode'] = cleandata['homesteadcreditqualificationcode'].apply(homesteadqualiticationcondition)
-cleandata["currentyeartotalassessment"] = cleandata["currentyeartotalassessment"].apply(lambda x: format_currency(x, currency="USD", locale="en_US"))
+cleandata['homesteadcreditqualificationcode'] = cleandata['homesteadcreditqualificationcode'].apply(
+    homesteadqualiticationcondition)
 
-#print columns & types
-print(cleandata.dtypes)
+#Formatting to Monetary numbers
+#cleandata["currentyeartotalassessment"] = cleandata["currentyeartotalassessment"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box2"] = cleandata["box2"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box4"] = cleandata["box4"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box5"] = cleandata["box6"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box6"] = cleandata["box6"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box7"] = cleandata["box7"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box8"] = cleandata["box8"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box9"] = cleandata["box9"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["box10"] = cleandata["box10"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
+#cleandata["totalchange"] = cleandata["totalchange"].apply(
+#    lambda x: format_currency(x, currency="USD", locale="en_US"))
 
 #Removing bad data points
+#These are common areas or open areas that are not owned by a resident but by the Association
 cleandata = cleandata.drop(60590243392)
 cleandata = cleandata.drop(60590243393)
 cleandata = cleandata.drop(60590243394)
@@ -87,18 +137,39 @@ cleandata = cleandata.drop(79790240744)
 cleandata = cleandata.drop(79790245914)
 
 #Modifying field value to be "TH"
+#There is one townhome that is listed as SF and the rest are blank
 cleandata.loc[cleandata["accountnumber"] == 60590243390, "housetype"] = "TH"
 cleandata.loc[cleandata["accountnumber"] == 60590254221, "housetype"] = "TH"
 cleandata.loc[cleandata["accountnumber"] == 60590254222, "housetype"] = "TH"
 cleandata.loc[cleandata["accountnumber"] == 60590254223, "housetype"] = "TH"
 cleandata.loc[cleandata["accountnumber"] == 60590254224, "housetype"] = "TH"
 
+#Updating year built 
+cleandata.loc[cleandata["accountnumber"] == 79790230860, "yearbuilt"] = "2011"
+cleandata.loc[cleandata["accountnumber"] == 79790230900, "yearbuilt"] = "2010"
+cleandata.loc[cleandata["accountnumber"] == 79790234258, "yearbuilt"] = "2013"
+
+cleandata['yearbuilt'] = cleandata['yearbuilt'].apply(yearcondition)
+
 #Debugging to see number of unique housetypes and how many 
 print(cleandata['housetype'].value_counts())
+
+#Debugging to see owner occupancy status of all homes
+print(cleandata['owneroccupancycode'].value_counts())
+
+#Debugging to see when homes were built
+print(cleandata['yearbuilt'].value_counts().sort_index(0))
+
 
 #Saving to CSV
 cleandata.to_csv('cleandata.csv')
 
 #printing data frame to screen
 cleandata
+
+cleandata["totalchange"] = pd.to_numeric(cleandata["totalchange"])
+averagechange = cleandata["totalchange"].mean()
+#print (averagechange)
+averagechange = "${:,.2f}".format(averagechange)
+print('The average home assessment change in Tanyard Springs is ', averagechange) 
 

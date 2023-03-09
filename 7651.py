aecd877dd0337d23d8c081eddcc4dd5bbd83f6d9
Mike Brown <mikeabrown2020@gmail.com>
2023-03-08 23:56:13 -0500

    added rates file and updated script

--- 7651.py
+++ 7651.py
@@ -3,6 +3,7 @@ author = "Michael Brown"
 
 import pandas as pd
 from limits import *
+from rates import *
 
 from warnings import simplefilter
 
@@ -13,6 +14,15 @@ tanyardTH = pd.read_csv('https://raw.githubusercontent.com/dataguy2020/PropertyT
 tanyardTH['year1difference'] = tanyardTH['box8'] - tanyardTH['box4']
 tanyardTH['year1countylimit'] = tanyardTH['box4'] + (tanyardTH['box4'] * annearundelcountylimit)
 tanyardTH['year1statelimit'] = tanyardTH['box4'] + (tanyardTH['box4'] * statelimit)
+tanyardTH['year1countydifference'] = tanyardTH['box8'] - tanyardTH['year1countylimit']
+tanyardTH['year1statedifference'] = tanyardTH['box8'] - tanyardTH['year1statelimit']
+
+tanyardTH.loc[tanyardTH['year1countydifference'] < 0, 'year1countycredit'] = 0
+tanyardTH.loc[tanyardTH['year1countydifference'] > 0, 'year1countycredit'] = (tanyardTH['year1countydifference'] * annearundelrate) / 100
+tanyardTH.loc[tanyardTH['year1statedifference'] < 0, 'year1statecredit'] = 0
+tanyardTH.loc[tanyardTH['year1statedifference'] > 0, 'year1statecredit'] = (tanyardTH['year1statedifference'] * annearundelrate) / 100
+
+
 
 # debugging
 print(tanyardTH.dtypes)
---
+++ rates.py
@@ -0,0 +1,2 @@
+staterate = 0.1120
+annearundelrate = .93300

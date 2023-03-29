a112a5ce64b0ba81e8959ee96f2e68f697158d0c
Mike Brown <mikeabrown2020@gmail.com>
2023-03-29 07:24:00 -0400

    upgraded line 27 and 32 to check for exemptclass status

--- functions.py
+++ functions.py
@@ -24,12 +24,12 @@ def taxcalculation(owneroccupied, homesteadcode, exemptclass, countyrealestate, 
     from rates import annearundelsolidwaste, annearundelstormwater
 
     if (exemptclass != "Blank"):
-        if (owneroccupied == "Yes" | owneroccupied == "No"):
+        if (owneroccupied in ("Yes", "No")):
             taxbill = annearundelsolidwaste + annearundelstormwater
             return taxbill
 
     if (exemptclass == "Blank"):
-        if (owneroccupied == "Yes"):
+        if (owneroccupied in ("Yes")):
             if (homesteadcode == "Approved" or homesteadcode == "Denied"):
                 taxbill = countyrealestate + staterealestate - countycredit - statecredit + annearundelsolidwaste + annearundelstormwater
                 return taxbill
